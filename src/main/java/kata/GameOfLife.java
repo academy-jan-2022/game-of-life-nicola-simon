7b52df9dc9e5339e6563ca24350011bf21cf3fb9
Simon <simonbesteiro@gmail.com>
2022-01-31 14:26:12 +0100

    first red

---
+++ src/main/java/kata/GameOfLife.java
@@ -0,0 +1,8 @@
+package kata;
+
+public class GameOfLife {
+
+    public boolean myMethod(String input) {
+        return null;
+    }
+}
---
+++ src/test/java/kata/GameOfLifeTest.java
@@ -0,0 +1,17 @@
+package kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GameOfLifeTest {
+
+    @Test
+    void return_empty_matrix_given_only_one_live_cell()
+    {
+        var gameOfLife = new GameOfLife();
+         var result = gameOfLife.myMethod("1");
+
+        assertEquals(result, "0");
+    }
+}
