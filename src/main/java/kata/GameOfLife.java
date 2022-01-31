fa44c04a077e6564d710d141927ecf0dfa0e07af
Simon <simonbesteiro@gmail.com>
2022-01-31 15:57:12 +0100

    green

--- src/main/java/kata/GameOfLife.java
+++ src/main/java/kata/GameOfLife.java
@@ -5,14 +5,14 @@ public class GameOfLife {
     public String checkNewGeneration(String input) {
 
         long countOfAliveCells = input.chars().filter(ch -> ch == '1').count();
-        if (countOfAliveCells == 1 || countOfAliveCells == 5) {
+        if (countOfAliveCells == 3 ) {
             return "0|0|0\n"
-                + "0|0|0\n"
+                + "0|1|0\n"
                 + "0|0|0";
         }
 
         return "0|0|0\n"
-            + "0|1|0\n"
+            + "0|0|0\n"
             + "0|0|0";
     }
 }
