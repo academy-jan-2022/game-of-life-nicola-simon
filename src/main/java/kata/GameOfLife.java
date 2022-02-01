0e348c9f96cc4fa990a798271ebea7d88bef418b
Nicola <nicola_rushton@outlook.com>
2022-02-01 08:10:28 +0000

    green

--- src/main/java/kata/GameOfLife.java
+++ src/main/java/kata/GameOfLife.java
@@ -3,7 +3,12 @@ package kata;
 public class GameOfLife {
 
     public boolean[][] checkNewGeneration(Boolean[][] input) {
-        boolean outputMatrix[][]= {{false}};
+
+        if (input[0].length == 2) {
+            return new boolean[][] {{false, false}};
+        }
+
+        boolean outputMatrix[][] = {{false}};
         return outputMatrix ;
     }
 }
