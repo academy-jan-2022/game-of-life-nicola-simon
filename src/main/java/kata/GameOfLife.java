ef8a3f6512dd39a0d1009402da9a539b8a1244bf
Simon <simonbesteiro@gmail.com>
2022-02-01 18:21:34 +0100

    refactor

--- src/main/java/kata/GameOfLife.java
+++ src/main/java/kata/GameOfLife.java
@@ -1,7 +1,5 @@
 package kata;
 
-import java.util.ArrayList;
-
 public class GameOfLife {
 
     public Boolean[][] checkNewGeneration(Boolean[][] input) {
@@ -10,13 +8,17 @@ public class GameOfLife {
         Boolean[][] newBoard = new Boolean[initialBoard.getNumberOfRows()][initialBoard.getNumberOfColumns()];
 
         for (int z = 0; z < initialBoard.getNumberOfRows(); z++){
-            for (int i = 0; i < initialBoard.getNumberOfColumns(); i++){
-                newBoard[z][i]=(isCellAlive(initialBoard,i,z));
-            }
+            rowConstructor(initialBoard, newBoard, z);
         }
 
         return newBoard;
 
+    }
+
+    private void rowConstructor(Board initialBoard, Boolean[][] newBoard, int z) {
+        for (int i = 0; i < initialBoard.getNumberOfColumns(); i++){
+            newBoard[z][i]=(isCellAlive(initialBoard,i, z));
+        }
     }
 
     private Boolean isCellAlive(Board board,int x, int y) {
