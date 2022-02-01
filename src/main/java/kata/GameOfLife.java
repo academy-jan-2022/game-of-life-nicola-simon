9e6d2ed0bb7cc0e5e2e548376400cff1abd7ec49
Simon <simonbesteiro@gmail.com>
2022-02-01 18:16:06 +0100

    refactor

--- src/main/java/kata/GameOfLife.java
+++ src/main/java/kata/GameOfLife.java
@@ -23,32 +23,6 @@ public class GameOfLife {
 
         return newBoard;
 
-
-        /*if (initialBoard.getNumberOfRows() == 3) {
-            return new Boolean[][]{
-                {isCellAlive(initialBoard,0,0), isCellAlive(initialBoard,1,0), isCellAlive(initialBoard,2,0)},
-                {isCellAlive(initialBoard,0,1), isCellAlive(initialBoard,1,1), isCellAlive(initialBoard,2,1)},
-                {isCellAlive(initialBoard,0,2), isCellAlive(initialBoard,1,2), isCellAlive(initialBoard,2,2)},
-            };
-        }
-
-        if (initialBoard.getNumberOfRows() == 2) {
-            return new Boolean[][]{
-                {isCellAlive(initialBoard,0,0), isCellAlive(initialBoard,1,0), isCellAlive(initialBoard,2,0)},
-                {isCellAlive(initialBoard,0,1), isCellAlive(initialBoard,1,1), isCellAlive(initialBoard,2,1)},
-            };
-        }
-
-        var foo = new ArrayList<Boolean>();
-
-        for (int i = 0; i < initialBoard.getNumberOfColumns(); i++){
-            foo.add(isCellAlive(initialBoard,i,0));
-        }
-
-        return new Boolean[][]{
-            foo.toArray(new Boolean[0])
-        };*/
-
     }
 
     private Boolean isCellAlive(Board board,int x, int y) {
