9d9951ed610556682459efd21ce4431439f6a7cb
Nicola <nicola_rushton@outlook.com>
2022-02-01 08:17:22 +0000

    green

--- src/main/java/kata/GameOfLife.java
+++ src/main/java/kata/GameOfLife.java
@@ -5,6 +5,10 @@ public class GameOfLife {
     public boolean[][] checkNewGeneration(Boolean[][] input) {
 
         if (input[0].length == 3) {
+            if (!input[0][0]) {
+                return new boolean[][] {{false, false, false}};
+            }
+
             return new boolean[][] {{false, true, false}};
         }
 
