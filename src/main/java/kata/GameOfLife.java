70069d8a4f32ff4f6a162049e8e407f6fbb5f13b
Nicola <nicola_rushton@outlook.com>
2022-02-01 13:27:24 +0000

    refactor

--- src/main/java/kata/GameOfLife.java
+++ src/main/java/kata/GameOfLife.java
@@ -5,43 +5,37 @@ public class GameOfLife {
     public boolean[][] checkNewGeneration(Boolean[][] input) {
         if (input.length == 3 && input[0][0]) {
             return new boolean[][] {
-                {true, false, false},
+                {isCellAlive(input), false, false},
                 getRow(input[1]),
                 getRow(input[2])};
         }
 
         int totalRowCount = getTotalRowCount(input);
-
         if (input.length == 3 ) {
 
-            if (totalRowCount > 2
-                && totalRowCount < 5
-            ){
-                return new boolean[][] {
-                    getRow(input[0]),
-                    {false, true, false},
-                    getRow(input[2])};
-            }
-
             return new boolean[][] {
                 getRow(input[0]),
-                getRow(input[1]),
+                {false, isCellAlive(input), false},
                 getRow(input[2])};
-        }
+            }
+
 
         if (input.length == 2 ) {
-            if (totalRowCount > 2 && totalRowCount < 5){
-                return new boolean[][] {
-                    getRow(input[0]),
-                    {false, true, false}};
-            }
-
             return new boolean[][] {
                 getRow(input[0]),
-                getRow(input[1])};
+                {false, isCellAlive(input), false}};
         }
 
         return new boolean[][]{getRow(input[0])};
+    }
+
+    private boolean isCellAlive(Boolean[][] input) {
+        int totalRowCount = getTotalRowCount(input);
+        if (totalRowCount > 2
+            && totalRowCount < 5) {
+            return true;
+        }
+        return false;
     }
 
     private int getTotalRowCount(Boolean[][] input) {
