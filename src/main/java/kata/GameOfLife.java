d187bea583a7856ea8007be573cba07babb41297
Nicola <nicola_rushton@outlook.com>
2022-02-01 08:28:04 +0000

    refactor

--- src/main/java/kata/GameOfLife.java
+++ src/main/java/kata/GameOfLife.java
@@ -4,12 +4,12 @@ public class GameOfLife {
 
     public boolean[][] checkNewGeneration(Boolean[][] input) {
 
-        if (input[0].length == 3) {
-            if (!input[0][0]) {
-                return new boolean[][] {{false, false, false}};
-            }
-
+        if (input[0].length == 3 && input[0][0]) {
             return new boolean[][] {{false, true, false}};
+        }
+
+        if (input[0].length == 3) {
+            return new boolean[][] {{false, false, false}};
         }
 
         if (input[0].length == 2) {
