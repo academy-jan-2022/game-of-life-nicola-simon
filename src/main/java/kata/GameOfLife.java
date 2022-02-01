fc292b73022b118c93f67227aef15884f2a3aad3
Simon <simonbesteiro@gmail.com>
2022-02-01 09:05:58 +0100

    green

--- src/main/java/kata/GameOfLife.java
+++ src/main/java/kata/GameOfLife.java
@@ -2,17 +2,8 @@ package kata;
 
 public class GameOfLife {
 
-    public String checkNewGeneration(String input) {
-
-        long countOfAliveCells = input.chars().filter(ch -> ch == '1').count();
-        if (countOfAliveCells == 3 ) {
-            return "0|0|0\n"
-                + "0|1|0\n"
-                + "0|0|0";
-        }
-
-        return "0|0|0\n"
-            + "0|0|0\n"
-            + "0|0|0";
+    public boolean[][] checkNewGeneration(Boolean[][] input) {
+        boolean outputMatrix[][]= {{false}};
+        return outputMatrix ;
     }
 }
--- src/test/java/kata/GameOfLifeTest.java
+++ src/test/java/kata/GameOfLifeTest.java
@@ -3,106 +3,18 @@ package kata;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class GameOfLifeTest {
-
-    @Test
-    void return_empty_matrix_given_only_one_live_cell()
-    {
-        var gameOfLife = new GameOfLife();
-         var result = gameOfLife.checkNewGeneration(
-             "0|0|0\n"
-             + "0|1|0\n"
-             + "0|0|0");
-
-        assertEquals("0|0|0\n"
-            + "0|0|0\n"
-            + "0|0|0", result);
-    }
-
-    @Test
-    void return_only_middle_cell_alive_given_cells_in_one_diagonal()
-    {
-        var gameOfLife = new GameOfLife();
-        var result = gameOfLife.checkNewGeneration(
-            "1|0|0\n"
-                + "0|1|0\n"
-                + "0|0|1"
-
-        );
-
-        assertEquals(
-            "0|0|0\n"
-                + "0|1|0\n"
-                + "0|0|0", result);
-    }
-
-    @Test
-    void return_only_middle_cell_alive_given_cells_in_reverse_diagonal()
-    {
-        var gameOfLife = new GameOfLife();
-        var result = gameOfLife.checkNewGeneration(
-            "0|0|1\n"
-                + "0|1|0\n"
-                + "1|0|0"
-
-        );
-
-        assertEquals(
-            "0|0|0\n"
-                + "0|1|0\n"
-                + "0|0|0", result);
-    }
-
-    @Test
-    void return_only_middle_cell_alive_given_cells_in_middle_row()
-    {
-        var gameOfLife = new GameOfLife();
-        var result = gameOfLife.checkNewGeneration(
-            "0|0|0\n"
-                + "1|1|1\n"
-                + "0|0|0"
-
-        );
-
-        assertEquals(
-            "0|0|0\n"
-                + "0|1|0\n"
-                + "0|0|0", result);
-    }
-
-    @Test
-    void return_empty_matrix_given_middle_and_corners_cells()
-    {
-        var gameOfLife = new GameOfLife();
-        var result = gameOfLife.checkNewGeneration(
-            "1|0|1\n"
-                + "0|1|0\n"
-                + "1|0|1"
-
-        );
-
-        assertEquals(
-            "0|0|0\n"
-                + "0|0|0\n"
-                + "0|0|0", result);
-    }
 
     @Test
     void return_empty_matrix_given_corners_only()
     {
         var gameOfLife = new GameOfLife();
-        var result = gameOfLife.checkNewGeneration(
-            "1|0|1\n"
-                + "0|0|0\n"
-                + "1|0|1"
+        Boolean currentMatrix [][] = {{true}};
+        var result = gameOfLife.checkNewGeneration(currentMatrix);
 
-        );
-
-        assertEquals(
-            "0|0|0\n"
-                + "0|0|0\n"
-                + "0|0|0", result);
+        assertFalse( result[0][0]);
     }
 
 }
